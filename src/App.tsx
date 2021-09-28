a2649738c9cc8889f4a525b99b64e6dac07b2fc8
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-09-28 10:24:38 -0300

    modal functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Deshboard } from "./components/Deshboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransctionModal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Deshboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
